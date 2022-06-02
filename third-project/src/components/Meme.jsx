40dca2b2bec6e7bee5fab4dface3801118c676d3
Leobardo Pineda <leobapineda@gmail.com>
2022-06-02 11:38:31 -0500

    Style: add both input value in meme img using useState object

--- third-project/src/components/Meme.jsx
+++ third-project/src/components/Meme.jsx
@@ -12,8 +12,10 @@ export default function Meme() {
   });
 
   const [allMemeImages, setallMemeImages] = useState(memeData);
+  const [memeText, setMemeText] = useState({topText:'', bottomText:''});
 
-  function getMemeImage() {
+  function getMemeImage(e) {
+    e.preventDefault()
     const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     let memeUrl = memesArray[randomNumber].url
@@ -28,28 +30,47 @@ export default function Meme() {
     })
 }
 
+  function handleMemeText(e) {
+    const {name, value} = e.target
+    setMemeText(memeText => {
+      return(
+        {
+          ...memeText,
+          [name] : value
+        }
+      )
+    })
+  }
+
   return (
     <main className="meme__container">
-      <div className="form">
+      <form className="form">
         <input
           className="meme__firstInput"
           type="text"
           placeholder={'Top Text'}
+          name='topText'
+          value={memeText.topText}
+          onChange={handleMemeText}
         />
         <input
           className="meme__secondInput"
           type="text"
           placeholder={'Bottom Text'}
+          name='bottomText'
+          value={memeText.bottomText}
+          onChange={handleMemeText}
+
         />
         <button className="meme__button" onClick={getMemeImage}>
           Get a new meme 😆
         </button>
-      </div>
+      </form>
       <div className="meme__img-container">
-        <img className="meme__img" src={meme.randomImage} alt="meme img" />
-        <div className="meme-text top" >one does not</div>
-        <div className="meme-text bottom" >walk into</div>
-      </div>
+          <img className="meme__img" src={meme.randomImage} alt="meme img" />
+          <div className="meme-text top" >{memeText.topText}</div>
+          <div className="meme-text bottom" >{memeText.bottomText}</div>
+        </div>
     </main>
   );
 }
